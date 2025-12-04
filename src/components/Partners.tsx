97163ad2261b93efeec9d66185761db4d59b3d58
koltech-mh <m.hanslik@koltech.com.pl>
2025-12-04 13:56:15 +0100

    src of logo partners without /src/

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -13,21 +13,21 @@ interface Partner {
 const partners: Partner[] = [
   {
     name: "FANUC",
-    logo: "../src/assets/img/logo/partners/fanuc_logo.png",
+    logo: "../assets/img/logo/partners/fanuc_logo.png",
     description: "Lider automatyzacji przemysłowej. Specjalizuje się w produkcji robotów przemysłowych, systemów CNC oraz technologii automatyzujących procesy produkcyjne.",
     expertise: "Robotyka Przemysłowa",
     link: "https://www.fanuc.eu/pl/pl",
   },
   {
     name: "ZAPAMET",
-    logo: "../src/assets/img/logo/partners/zapamet_logo.png",
+    logo: "../assets/img/logo/partners/zapamet_logo.png",
     description: "Specjalizuje się w automatyzacji produkcji, robotyce przemysłowej, obróbce metali i produkcji części metalowych.",
     expertise: "Spawalnictwo, Automatyzacja i Robotyka Przemysłowa",
     link: "https://www.zapamet.pl/",
   },
   {
     name: "Dassault Systèmes",
-    logo: "../src/assets/img/logo/partners/ds_logo_blue.png",
+    logo: "../assets/img/logo/partners/ds_logo_blue.png",
     description: "Światowy lider w dziedzinie projektowania 3D, cyfrowych makiet 3D i rozwiązań PLM. Dostawca platformy 3DEXPERIENCE, która umożliwia firmom zrównoważone innowacje.",
     expertise: "Platforma PLM",
     link: "https://www.3ds.com/",
