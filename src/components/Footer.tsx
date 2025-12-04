10f30a0062a40af30b68284ef9a31c3626d29b62
koltech-mh <m.hanslik@koltech.com.pl>
2025-12-04 12:43:12 +0100

    Footer now has actual data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Mail, Phone, MapPin, Linkedin, Twitter } from 'lucide-react';
+import { Mail, Phone, MapPin, Linkedin, Facebook, Instagram, Youtube } from 'lucide-react';
 import "./Footer.css";
 
 export function Footer() {
@@ -10,65 +10,77 @@ export function Footer() {
 
           {/* CONTACT INFO */}
           <div>
-            <h3 className="footer-title">Contact Us</h3>
+            <h3 className="footer-title">Kontakt</h3>
 
             <div className="footer-contact-list">
 
               <div className="footer-contact-item">
+                <MapPin className="footer-icon" />
+                <p className="footer-text">
+                  P.P.W. KOLTECH Sp. z.o.o. <br />
+                  Zakład Systemów Inżynierskich<br />
+                  Racibórz, 47-400<br/>
+                  ul. Malczewskiego 1
+                </p>
+              </div>
+
+              <div className="footer-contact-item">
                 <Mail className="footer-icon" />
-                <p className="footer-text">events@yourcompany.com</p>
+                <p className="footer-text">
+                  zsi@koltech.com.pl <br />
+                  plm_support@koltech.com.pl <br />
+                  marketing_zsi@koltech.com.pl
+                </p>
               </div>
 
               <div className="footer-contact-item">
                 <Phone className="footer-icon" />
-                <p className="footer-text">+1 (555) 123-4567</p>
-              </div>
-
-              <div className="footer-contact-item">
-                <MapPin className="footer-icon" />
-                <p className="footer-text">
-                  Tech Convention Center<br />
-                  San Francisco, CA 94105
-                </p>
+                <p className="footer-text">(+48) 32 415 35 09</p>
               </div>
 
             </div>
           </div>
 
-          {/* EVENT INFO */}
+          {/* EVENT INFO 
           <div>
-            <h3 className="footer-title">Event Information</h3>
+            <h3 className="footer-title">Informacje o Wydarzeniu</h3>
             <ul className="footer-list">
-              <li>Date: June 15, 2025</li>
-              <li>Time: 9:00 AM - 5:00 PM</li>
-              <li>Format: In-Person</li>
+              <li>Data: 23/10/2025</li>
+              <li>Godziny: 8:00 - 16:00</li>
+              <li>Miejsce: FANUC Polska, Wrocław</li>
               <li>Dress Code: Business Casual</li>
             </ul>
           </div>
-
+            */}
           {/* SOCIAL */}
           <div>
-            <h3 className="footer-title">Follow Us</h3>
+            <h3 className="footer-title">Śledź Nas</h3>
 
             <div className="footer-social">
-              <a href="#" className="footer-social-btn linkedin">
+              <a target="_blank" rel="noopener noreferrer" href="https://pl-pl.facebook.com/KOLTECHPLM" className="footer-social-btn facebook">
+                <Facebook className="footer-social-icon" />
+              </a>
+
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/koltech_plm_systems/" className="footer-social-btn instagram">
+                <Instagram className="footer-social-icon" />
+              </a>
+
+              <a target="_blank" rel="noopener noreferrer" href="https://pl.linkedin.com/company/wwwkoltechcompl" className="footer-social-btn linkedin">
                 <Linkedin className="footer-social-icon" />
               </a>
 
-              <a href="#" className="footer-social-btn twitter">
-                <Twitter className="footer-social-icon" />
+              <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/user/koltech/featured" className="footer-social-btn youtube">
+                <Youtube className="footer-social-icon" />
               </a>
             </div>
 
-            <p className="footer-desc">
-              Stay updated with the latest news and announcements about the PLM Innovation Summit.
-            </p>
+          
           </div>
 
         </div>
 
         <div className="footer-bottom">
-          <p>&copy; 2025 Your Company. All rights reserved. | PLM Innovation Summit 2025</p>
+          <p>&copy; 2025 KOLTECH. Wszystkie Prawa Zastrzeżone. | KOLTECH FORUM 2025</p>
         </div>
 
       </div>
